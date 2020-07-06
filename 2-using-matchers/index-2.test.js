fa254ef3dc4b41100d07e83bb3a7ddfc25bc579c
Samuel Njenga <snjenga704@gmail.com>
2020-07-06 21:26:21 +0300

    Added index-2.test.js which one distinguish between undefined, null, and false

---
+++ 2-using-matchers/index-2.test.js
@@ -0,0 +1,19 @@
+//Tests which lets you distinguish between undefined, null, and false
+//There are six falsy values in JavaScript: false, 0, '', null, undefined, and NaN. Any other thing is truthy
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+  });
+  
+  test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+  });
