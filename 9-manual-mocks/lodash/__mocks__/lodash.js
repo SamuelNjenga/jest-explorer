704fea99bd827d37a43ddeaaefeb9ca950d0b7c7
Samuel Njenga <snjenga704@gmail.com>
2020-07-12 12:43:00 +0300

    Modified lodash.js in mock directory by using jest.requireActual() function

--- 9-manual-mocks/lodash/__mocks__/lodash.js
+++ 9-manual-mocks/lodash/__mocks__/lodash.js
@@ -1,7 +1,8 @@
-const lodash = jest.genMockFromModule("lodash"); //Gets all methods of lodash and changes that for a jest mock
+//const lodash = jest.genMockFromModule("lodash"); //Gets all methods of lodash and changes that for a jest mock
 //undefined is default behavior for mocks
 
-
+//To ensure that a manual mock and its real implementation stay in sync, it might be useful to require the real module using jest.requireActual(moduleName) in your manual mock and amending it with mock functions before exporting it
+const lodash = jest.requireActual("lodash");
 lodash.repeat = () => {
     return "SSSSS"
 }
