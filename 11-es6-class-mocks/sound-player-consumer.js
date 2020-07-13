bc47941fd85396c7ca7711aa322f6249c6224fc6
Samuel Njenga <snjenga704@gmail.com>
2020-07-13 09:10:49 +0300

    Created 11-es6-class mocks directory and added automatic-mock.test.js which does automatic test for an  es6 class

---
+++ 11-es6-class-mocks/sound-player-consumer.js
@@ -0,0 +1,13 @@
+// sound-player-consumer.js
+const SoundPlayer = require('./sound-player');
+
+module.exports = class SoundPlayerConsumer {
+	constructor() {
+		this.soundPlayer = new SoundPlayer();
+	}
+
+	playSomethingCool() {
+		const coolSoundFileName = 'song.mp3';
+		this.soundPlayer.playSoundFile(coolSoundFileName);
+	}
+};
---
+++ 11-es6-class-mocks/sound-player.js
@@ -0,0 +1,10 @@
+// sound-player.js
+module.exports = class SoundPlayer {
+	constructor() {
+		this.foo = 'bar';
+	}
+
+	playSoundFile(fileName) {
+		console.log('Playing sound file ' + fileName);
+	}
+};
