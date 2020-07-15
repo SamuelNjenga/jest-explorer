6eb9ae75a2b3ec496606a5ce8c70977d74fb2008
Samuel Njenga <snjenga704@gmail.com>
2020-07-15 14:23:49 +0300

    Modified index-2.test.js which has a simpler syntax for testing rest api for the get method

--- 14-test-expressjs/index-2.test.js
+++ 14-test-expressjs/index-2.test.js
@@ -3,11 +3,20 @@ const app = require("./app");
 //Promise way -> That return is crucial, otherwise your tests will get stuck.
 //and there is No need to pass done to your test.
 describe("Test the root path", () => {
-  test("It should response the GET method", () => {
-    return request(app)
-      .get("/")
-      .then(response => {
-        expect(response.statusCode).toBe(200);
-      });
-  });
+    test("It should response the GET method", () => {
+        return request(app)
+            .get("/")
+            .then(response => {
+                expect(response.statusCode).toBe(200);
+            });
+    });
+
+    //Notice that without that return, the test will always pass
+    describe("Test the root path instance two", () => {
+        test("It should response the GET method", () => {
+            return request(app)
+                .get("/")
+                .expect(200);
+        });
+    });
 });
